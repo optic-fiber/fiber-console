c6e12077a915df48843ba3b32dc060c66c627285
cheroliv <cheroliv92@gmail.com>
2019-07-17 01:41:49 +0200

    _ _ _ _

--- src/main/groovy/com/cheroliv/fiber/FiberCommandLineRunner.groovy
+++ src/main/groovy/com/cheroliv/fiber/FiberCommandLineRunner.groovy
@@ -1,7 +1,9 @@
 package com.cheroliv.fiber
 
+
 import com.cheroliv.fiber.service.InterService
 import com.cheroliv.fiber.service.RecapService
+import com.cheroliv.fiber.view.MainFrame
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import org.springframework.boot.CommandLineRunner
@@ -10,17 +12,23 @@ import org.springframework.context.ApplicationContextAware
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
+import java.awt.EventQueue
+
 @Slf4j
 @Component
 @CompileStatic
 @Profile("!test")
-class FiberCommandLineRunner implements CommandLineRunner, ApplicationContextAware {
+class FiberCommandLineRunner implements
+        CommandLineRunner,
+        ApplicationContextAware {
 
     ApplicationContext applicationContext
 
     @Override
     void run(String... strings) throws Exception {
+
         if (!applicationContext.environment.activeProfiles.contains("test")) {
+
             String path = System.getProperty("user.home")
 
             InterService interService = applicationContext.getBean(InterService)
@@ -28,8 +36,15 @@ class FiberCommandLineRunner implements CommandLineRunner, ApplicationContextAwa
 
             interService.setUp()
             interService.importJsonFromFile interService.getFiberJsonFilePath(path)
-            recapService.processClasseurFeuilles(path)
-            interService.saveToJsonFile interService.getFiberJsonFilePath(path)
+//            recapService.processClasseurFeuilles(path)
+ //           interService.saveToJsonFile interService.getFiberJsonFilePath(path)
+
+
+            EventQueue.invokeLater(new Runnable() {
+                void run() {
+                    new MainFrame().setVisible(true)
+                }
+            })
         }
     }
 }
